f00d878cb163dbe81994e5d60757f7055b47eb94
Joey <josephdguillaume@gmail.com>
2021-03-21 22:09:58 -0400

    Updated example

--- study-sessions/self-study.js
+++ study-sessions/self-study.js
@@ -12,14 +12,3 @@ const roger = {
 bob.sayName.call(roger);
 const sayRoger = bob.sayName.bind(roger);
 sayRoger();
-
-const person = {
-  log() {
-    console.log(`${this}`);
-  },
-};
-
-person.log();
-
-function.bind(thisArg, ...args)
-sourceobject.method.bind(explicitContext, ...additionalArguments)
